a527e1890b40ca9585001ed01a2245075e174c8a
saranadhi-dev <saranaadithyan.dev@gmail.com>
2025-11-02 19:01:09 +0530

    updated dev mode to release mode

--- src/api.js
+++ src/api.js
@@ -1,1 +1,2 @@
-export const endpoint = "http://localhost:3000/api/equity/";
+// export const endpoint = "http://localhost:3000/api/equity/";
+export const endpoint = "https://portfolio-backend-a5i6.onrender.com/fetch-update"
